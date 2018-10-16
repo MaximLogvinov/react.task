82b202a4a7dd1dbfff30c54c85978157fb63764d
MaximLogvinov <33902979+MaximLogvinov@users.noreply.github.com>
2018-10-16 08:31:59 +0300

    delete needless

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Route,Router ,Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
-import ReduxToastr from 'react-redux-toastr'
 // local dependencies
 import Pages from './pages/index'
 import 'bootstrap/dist/css/bootstrap.css';
@@ -24,21 +23,11 @@ ReactDOM.render(
                         <PrivateRoute path='/welcome' component={Pages.PrivatePages.Welcome}/>
                         <PrivateRoute path='/settings' component={Pages.PrivatePages.Settings}/>
                     </Switch>
-                    <ReduxToastr
-                        timeOut={2000}
-                        newestOnTop={false}
-                        preventDuplicates
-                        position="top-left"
-                        transitionIn="fadeIn"
-                        transitionOut="fadeOut"
-                        progressBar
-                        closeOnToastrClick/>
                 </div>
             </Router>
         </Provider>
     ),
-    document.getElementById('root'),
-    console.log(store.getState(), store.getState().page.isAuth));
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
